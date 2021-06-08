14511375740ce47e25547110e6b8d31dbcf1ac8c
Antonio Venditti <antonio.venditti@codiceplastico.com>
2021-06-08 16:24:34 +0200

    clean architecture

---
+++ CleanArchitecture/CleanArchitecture.Domain/Entities/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public List<RichiesteAmicizia> RichiesteInCorso { get; set; }
+        public List<Persona> Amici { get; set; }
+        public Address address {get;set;}
+    }
+
+    public class Address
+    {
+        public string Citta { get; set; }
+        public string Indirizzo { get;set;}
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Domain/Entities/RichiesteAmicizia.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class RichiesteAmicizia
+    {
+        public int Id { get; set; }
+        public DateTime DataRichiesta { get; set; }
+        public Persona Amico { get; set; }
+    }
+}
